645693d49ff0233cc28d2387dd674829fd34d992
joaov <joaovale268@gmail.com>
2025-11-17 16:24:23 +0000

    Basic Login Screen

---
+++ app/src/main/java/com/example/computacaomovel/ui/screens/LoginScreen.kt
@@ -0,0 +1,70 @@
+package com.example.computacaomovel.ui.screens
+
+// LoginScreen.kt
+// Ecrã de login
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(
+    onLoginClick: (String, String) -> Unit = { _, _ -> },
+    onGoogleLoginClick: () -> Unit = {}
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(text = "Login", style = MaterialTheme.typography.headlineMedium)
+
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Button(
+                onClick = { onLoginClick(email, password) },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Entrar")
+            }
+
+            TextButton(onClick = onGoogleLoginClick) {
+                Text("Entrar com Google")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyScreenInteractivePreview() {
+    LoginScreen()
+}
+
